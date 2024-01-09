d4654925baee59a135126a31949e72a350b8d7fe
vencedoryosoy <hector.sf900@gmail.com>
2024-01-08 19:07:40 -0600

    Ejercicios Js terminados S5-1

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,1 +1,157 @@
+// number
+
 let edadHector = 25
+console.log(edadHector);
+
+let edadEric = 29
+console.log(edadEric);
+
+let edadMarisol = 24
+console.log(edadMarisol);
+
+let edadAbigail = 24
+console.log(edadAbigail);
+
+let edadEve = 25
+console.log(edadEve);
+
+let edadRafa = 25
+console.log(edadRafa);
+
+let edadRoberto = 25
+console.log(edadRoberto);
+
+let edadSinuhe = 25
+console.log(edadSinuhe);
+
+let edadElsa = 30
+console.log(edadElsa);
+
+let edadRomeo = 42
+console.log(edadHector);
+
+// string
+
+let nombreCompletoHector = "Hector Vicente Soto Flores"
+console.log(nombreCompletoHector);
+
+let nombreCompletoEric = "Eric Delgado Padilla"
+console.log(nombreCompletoEric);
+
+let nombreCompletoMarisol = "Marisol Lopez Franco"
+console.log(nombreCompletoMarisol);
+
+let nombreCompletoAbigail = "Abigail Sandoval Barcenas"
+console.log(nombreCompletoAbigail);
+
+let nombreCompletoEve = "Eveling Flores Delgado"
+console.log(nombreCompletoEve);
+
+let nombreCompletoRafa = "Rafael Ramirez Gomez"
+console.log(nombreCompletoRafa);
+
+let nombreCompletoRoberto = "Roberto Rivera Pacheco"
+console.log(nombreCompletoRoberto);
+
+let nombreCompletoSinuhe = "Sinuhe Alex Quintero Carvajal"
+console.log(nombreCompletoSinuhe);
+
+let nombreCompletoElsa = "Elsa Margarita Carvajal"
+console.log(nombreCompletoElsa);
+
+let nombreRealRomeo = "Anthony Santos"
+console.log(nombreRealRomeo);
+
+// booleanos
+
+let eresHector = true;
+console.log(eresHector);
+
+let conocesAMarisol = true;
+console.log(conocesAMarisol);
+
+let conocesAAbigail = true;
+console.log(conocesAAbigail);
+
+let conocesAEve = true;
+console.log(conocesAEve);
+
+let conocesARafa = true;
+console.log(conocesARafa);
+
+let conocesARoberto = true;
+console.log(conocesARoberto);
+
+let conocesASinuhe = true;
+console.log(conocesASinuhe);
+
+let conocesAElsa = false;
+console.log(conocesAElsa);
+
+let conocesARomeo = false;
+console.log(conocesARomeo);
+
+// Declarar variables numericas y realizar las operaciones aritmeticas basicas
+
+let sumaHectorMarisol = edadHector + edadMarisol;
+console.log("Las edades sumadas de Héctor y Marisol son iguales a:" + sumaHectorMarisol);
+
+let restaHectorMarisol = edadHector - edadMarisol
+console.log("La edad de Hector menos la edad de Marisol es igual a:" + restaHectorMarisol);
+
+let productoHectorMarisol = edadHector * edadMarisol;
+console.log("El producto de las edades de Héctor y Marisol es iguales a:" + productoHectorMarisol);
+
+let divisionHectorMarisol = edadHector / edadMarisol;
+console.log("Las edad de Hector entre la edad de Marisol es igual a:" + divisionHectorMarisol);
+
+// mensajes concatenados
+
+let amigosUniversidad = [nombreCompletoEric, nombreCompletoRafa]
+console.log("Mis mejores amigos de la universidad son: " + amigosUniversidad);
+
+let musicaHector =[nombreRealRomeo, nombreCompletoElsa]
+console.log("Mis músicos favoritos en este momento son: " + musicaHector)
+
+let amigosDesconocidos = [nombreCompletoRafa, nombreCompletoEric, nombreCompletoSinuhe]
+console.log("Mi gemelo: " + nombreCompletoRoberto + " " + ", no conoce a mis mejores amigos de la univeridad: " + amigosDesconocidos)
+
+let ingenierasMineras = [nombreCompletoEve, nombreCompletoMarisol]
+console.log("Mi colega: " + nombreCompletoAbigail + " , es es Ingeniera Industrial y no conoce a mis colegas: " + ingenierasMineras+ ", que son ingenieras mineras"); 
+
+let personasConocidas = [nombreCompletoAbigail, nombreCompletoEric, nombreCompletoMarisol, nombreCompletoRafa, nombreCompletoRoberto, nombreCompletoSinuhe, nombreCompletoEve]
+let personasDesconocidas = [nombreRealRomeo, nombreCompletoElsa]
+console.log("Conozco a: " + personasConocidas + ", pero no conozco a: " + personasDesconocidas); 
+
+// convertir tipos de variables
+
+let edadPaty = 25;
+let edadLily = 24
+let edadArgelia = 25
+let edadMarlen = 25
+let edadVicente = 25
+
+let edadBooleanPaty = Boolean(edadPaty);
+console.log(edadBooleanPaty);
+
+let edadStringLily = String(edadLily);
+console.log(edadStringLily);
+
+let edadBooleanArgelia = Boolean(edadArgelia);
+console.log(edadBooleanArgelia);
+
+let edadStringMarlen = String(edadMarlen);
+console.log(edadStringMarlen);
+
+let edadBooleanVicente = Boolean(edadVicente);
+console.log(edadBooleanVicente);
+
+
+
+
+
+
+
+
+
+
