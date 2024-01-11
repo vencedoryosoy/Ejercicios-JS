d78fd606bb889592da7892f0066a5241854bb78f
vencedoryosoy <hector.sf900@gmail.com>
2024-01-11 12:51:43 -0600

    Ejercicios sin terminar

---
+++ Loops.js
@@ -0,0 +1,24 @@
+/**
+ * Calculate the sum of all resistors connected in series.
+Examples:
+- `sumResitance([-1,5,6,3])` should return `"15 ohms"`. (|−1| + 5 + 6 + 3 = 15)
+- `sumResitance([14,3.5,6])` should return `'23.5 ohms'`. (14 + 3.5 + 6 = 23.5)
+- `sumResitance([8,15,100])` should return `'123 ohms'`. (8 + 15 + 100 = 123)
+
+Note: This approach uses the absolute value of each resistance to ensure all values are positive.
+et resistance = [-1,5,6,3]
+
+resistance.forEach(i => console.log (Math.sqrt(i*i)))
+
+ */
+
+
+
+
+
+
+
+
+
+
+
