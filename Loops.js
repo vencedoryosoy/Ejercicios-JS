8466bde2b733214dde970005706cfc64a398358c
vencedoryosoy <hector.sf900@gmail.com>
2024-01-12 14:43:03 -0600

    Práctica de lógica, incompleta

--- Loops.js
+++ Loops.js
@@ -6,11 +6,107 @@ Examples:
 - `sumResitance([8,15,100])` should return `'123 ohms'`. (8 + 15 + 100 = 123)
 
 Note: This approach uses the absolute value of each resistance to ensure all values are positive.
-et resistance = [-1,5,6,3]
+let resistance = [-1,5,6,3]
 
 resistance.forEach(i => console.log (Math.sqrt(i*i)))
 
+let resistencias = [-1, 5, 6, 3];
+let suma = 0;
+
+for (i = 0 ; i < resistencias.length; i++){
+    suma += resistencias[i];
+}
+
+console.log(suma)
+
+var resistance = [-1,5,6,3]
+
+resistance.forEach(i => console.log (Math.sqrt(i*i))
+
+
+)
+
  */
+
+/**
+ * Display online status of a list of users
+ * 
+ * onlineStatus (["Usuario1", "Usuario2", "Usuario n"])
+ * 
+ * Expected output
+ * 
+ * 'Usuario1', usuario2 and n more are online
+ * 
+ */
+
+//1. Obtener la cantidad total de usuarios
+//2. Calcular usuarios restantes eliminando del total los primeros 2
+//3. Obtener los primeros 2
+// 4. Imprimir mensaje
+
+const users = ['mocking99', 'J0eyPunch', 'glassedFer', 'anon123'];
+function getLength(arr) {
+    return arr.length;
+}
+
+function calcRemaining (total){
+    return total -2;
+}
+
+function getUsers (arr)
+const users = arr[0] + arr[1];
+return users;
+
+function onlineStatus(arr) {
+    const total = getLength(arr);
+    const rest = calcRemaining(total);
+    const firstUsers = getUsers(arr);
+
+    console.log(`Users: ${firstUsers}, and ${rest} are online`);
+}
+
+onlineStatus(users);
+
+////////////////////////////////
+
+const onlineStatus = arr => {
+    if (arr.length >= 3) {
+        const firstUsers = getUsers(arr);
+        console.log(`Users: ${firstUsers}, and ${arr.length -2} are online`);
+
+    } else {
+        console.log(`users: ${arr.join(',')} are online`);
+    }
+}
+
+onlineStatus(users);
+
+// Write a function to determinate if an array is positively dominant...
+
+// 1. Loop por cada elemento
+// 2. Total de positivos
+// 3. Cantidad total y dividir sobre 2
+// 4. Comparamos e imprimir boolean
+
+const numArr = [-1000, -1, 50, 1, 2, 5];
+const negativArr = [-100, -1, -50, -1, 5, 2]
+
+function positiveArr(arr) {
+    let positiveCount = 0;
+    arr.forEach(num) => 
+    {
+        if (num > 0) {
+            positiveCount++;
+                }
+    }
+    const mid = arr.length /2;
+    return positiveCount > mid;
+}
+
+const res = positiveArr(negativeArr);
+console.log(res)
+
+
 
 
 
---
+++ Practica_logica.js
@@ -0,0 +1,24 @@
+/**
+ * User profile
+Write a program that prompts a user for their data: username, age, and a list of their favorite movies. Store the information and then showcase it in the console. Take note that the output for the films should add a small message like: 'The film {film} is one of my favorites'.
+
+Highest number
+Write a program that asks for 10 numbers. Using logical operators and any other javascript functions/structures you've seen before, determine and output the highest of those numbers.
+
+Alarm
+Write a program that asks a user for the amount of seconds needed until an alarm (message) is executed alongside a text to show once those seconds have passed in real time.
+
+Result example: "Time for bed after 10 seconds".
+
+Palindrome
+Write a program that prompts for a word or sentence (it can be capitalized, have spaces and punctuation). Figure out if the sentence/word is a palindrome or not. Ignoring punctuation, spaces and capitalized letters.
+
+Factorial
+Write a program that prompts for an intenger number n. Where  1 <= n. Solve this using recursion.
+
+Flat array
+Write a program that takes the following nested matrix and flattens it (makes it a 1D array). Use any type of algorithm you want like callbacks, recursion, etc...
+
+let multiDimension = [1, [2, 3, [4, 5, [6]]]];
+
+ */
